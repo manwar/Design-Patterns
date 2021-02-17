e6df93df43925833a788f06587783e3943e450e3
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2021-02-17 23:13:45 +0000

    - Updated SYNOPSIS for Prototype design pattern.

--- 15-prototype/lib/PrototypePattern.pm
+++ 15-prototype/lib/PrototypePattern.pm
@@ -8,6 +8,14 @@ PrototypePattern - Demo Prototype design pattern.
 
 =head1 SYNOPSIS
 
+    use ShapeCache;
+
+    my $cache = ShapeCache->new;
+
+    print $cache->getShape(1)->type; # Circle
+    print $cache->getShape(2)->type; # Square
+    print $cache->getShape(3)->type; # Rectangle
+
 =cut
 
 1;
